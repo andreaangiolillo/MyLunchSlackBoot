b6009f4a20e20338251c5aec735ceb93ba4a0083
andreaangiolillo <andrea.angiolillo@mongodb.com>
2022-12-08 23:19:09 +0000

    added viper

--- cmd/myLunchSlackBot/main.go
+++ cmd/myLunchSlackBot/main.go
@@ -1,21 +1,40 @@
-package myLunchSlackBot
+package main
 
 import (
 	"fmt"
 	"github.com/slack-go/slack"
+	"github.com/spf13/viper"
 )
 
+func newViper() error {
+	viper.SetConfigType("json")
+	viper.SetConfigName("config")
+	viper.AddConfigPath("./configs/")
+
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		return err
+	}
+
+	return nil
+}
+
 func main() {
-	api := slack.New("YOUR_TOKEN_HERE")
-	// If you set debugging, it will log all requests to the console
-	// Useful when encountering issues
-	// slack.New("YOUR_TOKEN_HERE", slack.OptionDebug(true))
-	groups, err := api.GetUserGroups(slack.GetUserGroupsOptionIncludeUsers(false))
+	if err := newViper; err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	token := viper.GetString("slack.token")
+	if token == "" {
+		panic("Missing slack token")
+	}
+
+	api := slack.New(token)
+	user, err := api.GetUserInfo("U04ETL260C9")
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		return
 	}
-	for _, group := range groups {
-		fmt.Printf("ID: %s, Name: %s\n", group.ID, group.Name)
-	}
+
+	fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
 }
